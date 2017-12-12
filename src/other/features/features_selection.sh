707ab1e477b10d6f1adadcb796366431bd6e3844
Manuel Lobo <Hellsbath@gmail.com>
2017-12-12 13:19:14 +0000

    Delete features_selection.sh

--- src/other/features/features_selection.sh
+++
@@ -1,3 +0,0 @@
-python src/main.py train --goldstd hpo_train --models models/hpo_train
-python src/main.py test --goldstd hpo_test -o pickle data/results_hpo_train --models models/hpo_train
-python src/evaluate.py evaluate hpo_test --results data/results_hpo_train --models models/hpo_train
